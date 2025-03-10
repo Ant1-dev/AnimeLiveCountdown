0d1639f4724492b2e46787641c732031f6160f55
Ant1 <47747877+Ant1-dev@users.noreply.github.com>
2025-03-10 12:23:01 -0400

    updated frontend to now be a SPA rather than a SSR and edited cors config for specific urls

--- animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/config/CorsConfig.java
+++ animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/config/CorsConfig.java
@@ -1,4 +1,32 @@
 package com.Toine.animeCountdownBackend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // specific origins:
+        config.addAllowedOrigin("https://your-angular-app.vercel.app");
+        config.addAllowedOrigin("http://localhost:4200");
+
+        // allow all HTTP methods
+        config.addAllowedMethod("*");
+
+        // allow all headers
+        config.addAllowedHeader("*");
+
+        // allow credentials like cookies, authentication in the future???
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
 }
