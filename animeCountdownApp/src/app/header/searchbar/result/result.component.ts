7d7371aafbd5a6a69b9d4ff865b0da98b2ff9a7e
Ant1 <47747877+Ant1-dev@users.noreply.github.com>
2025-03-22 23:37:26 -0400

    search bar now takes to media associated page

--- animeCountdownApp/src/app/header/searchbar/result/result.component.ts
+++ animeCountdownApp/src/app/header/searchbar/result/result.component.ts
@@ -1,15 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Media } from '../../../schedule.model';
 import { CommonModule } from '@angular/common';
+import { MediaInfoService } from '../../../services/media.info.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-result',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './result.component.html',
   styleUrl: './result.component.css'
 })
 export class ResultComponent {
   result = input<Media>();
+  mediaInfoServe = inject(MediaInfoService);
 
-
+  retrieveId() {
+    this.mediaInfoServe.getMediaId(this.result()!.id);
+  }
 }
