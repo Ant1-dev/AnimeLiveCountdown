9d4e85025a93111ba26b509a1813de2016950c25
Ant1 <47747877+Ant1-dev@users.noreply.github.com>
2025-05-25 22:31:10 -0400

    Created Delete, Post mapping for FavoriteMediaController

--- animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/controllers/FavoriteMediaController.java
+++ animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/controllers/FavoriteMediaController.java
@@ -1,25 +1,54 @@
 package com.Toine.animeCountdownBackend.controllers;
 
 import com.Toine.animeCountdownBackend.models.postgreEntities.MediaEntity;
+import com.Toine.animeCountdownBackend.models.postgreEntities.UserEntity;
 import com.Toine.animeCountdownBackend.repositories.FavoriteMediaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Toine.animeCountdownBackend.repositories.MediaRepository;
+import com.Toine.animeCountdownBackend.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth/fav-media")
+@RequestMapping("/api/auth/favorite")
 public class FavoriteMediaController {
+    private final MediaRepository mediaRepository;
+    private final UserRepository userRepository;
     private FavoriteMediaRepository favRepository;
 
-    public FavoriteMediaController(FavoriteMediaRepository favRepository) {
+    public FavoriteMediaController(FavoriteMediaRepository favRepository, MediaRepository mediaRepository, UserRepository userRepository) {
         this.favRepository = favRepository;
+        this.mediaRepository = mediaRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/{userId}")
-    public List<MediaEntity> getFavoriteMedia(@PathVariable Long userId) {
-        return favRepository.findFavMedia(userId);
+    public List<MediaEntity> getAllFavoriteMedia(@PathVariable Long userId) {
+        return favRepository.findAllFavMedia(userId);
+    }
+
+    @PostMapping("/{userId}/{mediaId}")
+    public ResponseEntity<String> addFavoriteMedia(@PathVariable Long userId, @PathVariable Long mediaId) {
+        Optional<UserEntity> user = userRepository.findById(userId);
+        Optional<MediaEntity> media = mediaRepository.findById(mediaId);
+
+        boolean success = favRepository.addFavoriteMedia(user, media, Instant.now());
+        if (success)
+            return ResponseEntity.ok("Successfully added to favorites!");
+        else
+            return ResponseEntity.badRequest().body("Could not be added to favorites.");
+    }
+
+    @DeleteMapping("/{mediaId}")
+    public ResponseEntity<String> deleteFavoriteMedia(@PathVariable Long mediaId) {
+        boolean success = favRepository.removeFavoriteMedia(mediaId);
+
+        if (success)
+            return ResponseEntity.ok("Successfully removed from favorites!");
+        else
+            return ResponseEntity.badRequest().body("Could not remove from favorites.");
     }
 }
--- animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/repositories/FavoriteMediaRepository.java
+++ animeCountdownBackend/src/main/java/com/Toine/animeCountdownBackend/repositories/FavoriteMediaRepository.java
@@ -2,16 +2,31 @@ package com.Toine.animeCountdownBackend.repositories;
 
 import com.Toine.animeCountdownBackend.models.postgreEntities.FavoriteMediaEntity;
 import com.Toine.animeCountdownBackend.models.postgreEntities.MediaEntity;
+import com.Toine.animeCountdownBackend.models.postgreEntities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoriteMediaRepository extends JpaRepository<FavoriteMediaEntity, Long> {
 
     @Query("SELECT fm.media FROM FavoriteMediaEntity fm WHERE fm.id = :userId")
-    List<MediaEntity> findFavMedia(@Param("userId") Long userId);
+    List<MediaEntity> findAllFavMedia(@Param("userId") Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO FavoriteMediaEntity (user, media, addedDate) VALUES(:user, :media, :addedDate)")
+    boolean addFavoriteMedia(@Param("user")Optional<UserEntity> user, @Param("media") Optional<MediaEntity> media, @Param("addedDate")Instant addedDate);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM FavoriteMediaEntity fm WHERE fm.id = :favoriteMediaId")
+    boolean removeFavoriteMedia(@Param("mediaId") Long favoriteMediaId);
 }
